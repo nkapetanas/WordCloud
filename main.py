6967949a3c598eeaffe7e146b8e16bf50dc8fdb4
Nick Kapetanas <nkapetanas@outlook.com>
2019-12-25 00:46:52 +0200

    refactored worldcloud implementation

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ DATASET_PATH_TRAIN = "C:/Users/Delta/PycharmProjects/WordCloud/dataset/train.csv
 
 def read_dataset(dataset):
     df = pd.read_csv(dataset)
-    df.head()
     return df
 
 
@@ -22,7 +21,7 @@ def create_wordClouds(text, category):
     stopwords = set(STOPWORDS)
 
     # Create and generate a word cloud image:
-    wc = WordCloud(stopwords=stopwords, background_color="white", max_font_size=50, max_words=200)
+    wc = WordCloud(stopwords=stopwords, background_color="white", max_font_size=50, max_words=250)
 
     counts_all = Counter()
 
@@ -32,7 +31,6 @@ def create_wordClouds(text, category):
 
     wc.generate_from_frequencies(counts_all)
     worldcloud_toImage(wc, category)
-    # wc.to_file('wc.png')
 
 
 def plot_wordcloud(wordcloud):
@@ -47,13 +45,19 @@ def get_Content_Text(df):
     return words
 
 
-def remove_num(text):
-    textWithoutNumbers = ''.join([i for i in text if not i.isdigit()])
-    return textWithoutNumbers
-
-
 def worldcloud_toImage(wordcloud, category):
     wordcloud.to_file(category + ".png")
+
+
+def clean_data(dataframe):
+    dataframe['Content'] = dataframe['Content'].str.lower()
+    all_content = dataframe['Content'].str.split(' ')
+    all_text_cleaned = []
+    for text in all_content:
+        text = [x.strip(string.punctuation) for x in text]
+        all_text_cleaned.append(text)
+    text = [" ".join(text) for text in all_text_cleaned]
+    return text
 
 
 df = read_dataset(DATASET_PATH_TRAIN)
@@ -63,19 +67,15 @@ df_Entertainment = df.loc[df['Label'] == "Entertainment"]
 df_Health = df.loc[df['Label'] == "Health"]
 df_Technology = df.loc[df['Label'] == "Technology"]
 
-df_Business['Content'] = df_Business['Content'].str.lower()
-all_business = df_Business['Content'].str.split(' ')
-
-all_business_cleaned = []
-
-for text in all_business:
-    text = [x.strip(string.punctuation) for x in text]
-    all_business_cleaned.append(text)
-
-text_business = [" ".join(text) for text in all_business_cleaned]
+text_business = clean_data(df_Business)
+text_entertainment = clean_data(df_Entertainment)
+text_health = clean_data(df_Health)
+text_technology = clean_data(df_Technology)
 
 create_wordClouds(text_business, "Business")
-
+create_wordClouds(text_entertainment, "Entertainment")
+create_wordClouds(text_health, "Health")
+create_wordClouds(text_technology, "Technology")
 
 # df_Business['Content'] = df_Business['Content'].apply(lambda x : x.strip().capitalize())
 
@@ -87,19 +87,3 @@ create_wordClouds(text_business, "Business")
 # df_Business['Content'] = df_Business['Content'].str.replace('▯', '')
 # df_Business['Content'] = df_Business['Content'].str.replace("'", '')
 # df_Business['Content'] = df_Business['Content'].str.replace(",", "")
-
-# text = get_Content_Text(df_Business)
-
-
-# removing other characters
-def remove_u(text):
-    text = text.replace('_', '')
-    text = text.replace('?', '')
-    text = text.replace('•', '')
-    text = text.replace("@", '')
-    text = text.replace('▯', '')
-    text = text.replace("'", '')
-    text = text.replace(",", "")
-    return text
-
-# worldCloud_toImage(word_cloud, "Business")
