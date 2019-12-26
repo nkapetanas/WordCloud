0faf9dc83336a3bccb1fd9302e9458a35aaf00fd
Nick Kapetanas <nkapetanas@outlook.com>
2019-12-26 21:58:34 +0200

    initial implementation of text classification - added hashing vectorizer

---
+++ textClassification.py
@@ -0,0 +1,40 @@
+import string
+
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+from nltk.corpus import stopwords
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import HashingVectorizer
+
+DATASET_PATH_TRAIN = "C:/Users/Delta/PycharmProjects/WordCloud/dataset/train.csv"
+DATASET_PATH_TEST = "C:/Users/Delta/PycharmProjects/WordCloud/dataset/test_without_labels.csv"
+
+
+def read_dataset(dataset):
+    df = pd.read_csv(dataset)
+    return df
+
+
+def clean_data(dataframe):
+    dataframe['Content'] = dataframe['Content'].str.lower()
+    all_content = dataframe['Content'].str.split(' ')
+    all_text_cleaned = []
+    for text in all_content:
+        text = [x.strip(string.punctuation) for x in text]
+        all_text_cleaned.append(text)
+    text = [" ".join(text) for text in all_text_cleaned]
+    return text
+
+
+train_data = read_dataset(DATASET_PATH_TRAIN)
+test_data = read_dataset(DATASET_PATH_TEST)
+
+# cleaning of data
+train_data_cleaned = clean_data(train_data)
+test_data_cleaned = clean_data(test_data)
+
+# create the transform
+vectorizer = HashingVectorizer(decode_error='ignore', n_features=2 ** 18, alternate_sign=False)
+
+# encode document: encodes the sample document as a 2 ** 18-element sparse array
+vectorizer.fit_transform(train_data_cleaned)
