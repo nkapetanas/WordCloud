d53107a95ee8bb972da2d0a7dc7c86fd949df680
Nick Kapetanas <nkapetanas@outlook.com>
2020-02-13 23:06:50 +0200

    text classification added export prediction to csv

--- textClassification.py
+++ textClassification.py
@@ -1,23 +1,17 @@
 import string
 
-import pandas as pd
-import nltk
-
-from sklearn.linear_model import SGDClassifier
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from nltk.tokenize import RegexpTokenizer
 import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import HashingVectorizer, CountVectorizer, TfidfTransformer
 
 DATASET_PATH_TRAIN = "C:/Users/Delta/PycharmProjects/WordCloud/dataset/train.csv"
 DATASET_PATH_TEST = "C:/Users/Delta/PycharmProjects/WordCloud/dataset/test_without_labels.csv"
@@ -49,6 +43,12 @@ def clean_data(dataframe):
         lambda x: ' '.join([item for item in x.split() if item not in stop]))
 
 
+def createCSV(prediction, csvName):
+    np.savetxt(csvName,
+               np.dstack((np.array(test_data["Id"].values), prediction))[0], "%d,%d",
+               header="Id,Predicted")
+
+
 def calculate_metrics(y_actual, y_predicted):
     accuracy = accuracy_score(y_actual, y_predicted)
     precision = precision_score(y_actual, y_predicted, average='micro')
@@ -56,37 +56,49 @@ def calculate_metrics(y_actual, y_predicted):
     f1 = f1_score(y_actual, y_predicted, average='micro')
 
     return accuracy, precision, recall, f1
-
-
 train_data = read_dataset(DATASET_PATH_TRAIN)
-test_data = read_dataset(DATASET_PATH_TEST)
 
+test_data = read_dataset(DATASET_PATH_TEST)
 classifier = {
     'SGD': SGDClassifier(max_iter=5)
 }
 # cleaning of data
 clean_data(train_data)
+
 clean_data(test_data)
-
 Encoder = LabelEncoder()
-train_data['Label_Encoded'] = Encoder.fit_transform(train_data['Label'])
-
-classes = np.unique(train_data['Label_Encoded'])
 
 # list_of_train_data = np.array_split(train_data, 5)
 
+train_data['Label_Encoded'] = Encoder.fit_transform(train_data['Label'])
 # separating features for our model from the target variable
 x_train_data = train_data['Content']
+
 y_train_data = train_data['Label_Encoded']
+
+test_data = test_data['Content']
 
 cls_stats = {}
 
-kfold = KFold(n_splits=5, random_state=42, shuffle=True)
+kfold = KFold(n_splits=2, random_state=42, shuffle=True)
 
 fold = 0
 
 vectorizer = HashingVectorizer(decode_error='ignore', n_features=2 ** 18, alternate_sign=False,
                                token_pattern=TOKENS_ALPHANUMERIC)
+scores_svm_accuracy = []
+scores_svm_precision = []
+scores_svm_recall = []
+
+scores_svm_f1 = []
+scores_rf_accuracy = []
+scores_rf_precision = []
+scores_rf_recall = []
+
+scores_rf_f1 = []
+sgd_classifier = SGDClassifier(max_iter=1000, loss='hinge')
+
+rand_forest_classifier = RandomForestClassifier(n_jobs=-1, max_depth=500)
 
 for train_index, test_index in kfold.split(x_train_data):
     fold += 1
@@ -95,29 +107,44 @@ for train_index, test_index in kfold.split(x_train_data):
     x_train_k, x_test_k = x_train_data.iloc[train_index], x_train_data.iloc[test_index]
     y_train_k, y_test_k = y_train_data.iloc[train_index], y_train_data.iloc[test_index]
 
-
     X_train = vectorizer.fit_transform(x_train_k)
     X_test = vectorizer.fit_transform(x_test_k)
 
-    sgd_classifier = SGDClassifier(max_iter=1000, loss='hinge')
     sgd_classifier.fit(X_train, y_train_k)
     predictedValues = sgd_classifier.predict(X_test)
 
-    print("SGDClassifier metrics")
     accuracy, precision, recall, f1 = calculate_metrics(y_test_k, predictedValues)
-    print("accuracy:" + str(accuracy))
-    print("precision:" + str(precision))
-    print("recall:" + str(recall))
-    print("f1:" + str(f1))
+    scores_svm_accuracy.append(accuracy)
+    scores_svm_precision.append(precision)
+    scores_svm_recall.append(recall)
+    scores_svm_f1.append(f1)
 
-    rand_forest_classifier = RandomForestClassifier(n_jobs=-1, max_depth=500)
     rand_forest_classifier.fit(X_train, y_train_k)
     predictedValues_rand_forest = rand_forest_classifier.predict(X_test)
 
-    print("Random Forest metrics")
     accuracy, precision, recall, f1 = calculate_metrics(y_test_k, predictedValues_rand_forest)
-    print("accuracy:" + str(accuracy))
-    print("precision:" + str(precision))
-    print("recall:" + str(recall))
-    print("f1:" + str(f1))
+    scores_rf_accuracy.append(accuracy)
+    scores_rf_precision.append(precision)
+    scores_rf_recall.append(recall)
+    scores_rf_f1.append(f1)
+print("SGDClassifier metrics")
+print("Accuracy:" + str(np.mean(scores_svm_accuracy)))
+print("Precision:" + str(np.mean(scores_svm_precision)))
+print("Recall:" + str(np.mean(scores_svm_recall)))
 
+print("F1:" + str(np.mean(scores_svm_f1)))
+print("Random Forest metrics")
+print("Accuracy:" + str(np.mean(scores_rf_accuracy)))
+print("Precision:" + str(np.mean(scores_rf_precision)))
+print("Recall:" + str(np.mean(scores_rf_recall)))
+
+print("F1:" + str(np.mean(scores_rf_f1)))
+test_data = vectorizer.fit_transform(test_data)
+predictedValues = sgd_classifier.predict(test_data)
+
+
+predictedValues_rand_forest = rand_forest_classifier.predict(test_data)
+
+
+createCSV(predictedValues, "testSet_categories.csv")
+createCSV(predictedValues_rand_forest, "testSet_categories2.csv")
